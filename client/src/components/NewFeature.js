a744c5132579cbb84c38dfbeb8fa3f47cf6fcde0
Vamsi Krishna <vamsikrishna@example.com>
2024-07-08 10:49:31 +0530

    Added NewFeature component and integrated it into App.js

---
+++ client/src/components/NewFeature.js
@@ -0,0 +1,12 @@
+// client/src/components/NewFeature.js
+import React from 'react';
+
+const NewFeature = () => {
+  return (
+    <div>
+      <h2>This is a new feature!</h2>
+    </div>
+  );
+}
+
+export default NewFeature;
